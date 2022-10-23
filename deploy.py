cc2e2024a4708b743633ef4921d326c379927df2
Simon Legah <legah2000@gmail.com>
2022-10-23 19:13:18 +0000

    committing new changes

--- deploy.py
+++ deploy.py
@@ -1,4 +1,4 @@
 #deploying a python web application.
-echo "echo hello world i love you"
-python ('hello world')
+echo "echo hello world i love you will you mary me"
+python('hello world')
 #python script
